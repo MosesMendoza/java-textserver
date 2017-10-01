b3999cf3ddf40cb3cdcae64bc630667c1e2e4107
Moses Mendoza <moses@puppet.com>
2017-10-01 13:47:40 -0700

    Add method to retrieve from URLs concurrently
    retrieveContentAtURLs uses a few of the concurrency features of java 8 to
    perform concurrent retrieval of the content at the supplied URLs.
    
    We create a task to execute for each URL and leverage an execution service to
    run them for us. We use the CompletableFuture allOf method to block until
    completion, and then aggregate the results.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- src/main/java/textserver/TextClient.java
+++ src/main/java/textserver/TextClient.java
@@ -1,15 +1,20 @@
 package textserver;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.lang.InterruptedException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-// An http client class to retrieve text from a given set of URLS
 
 public class TextClient {
 
@@ -20,6 +25,10 @@ public class TextClient {
    * @return String the string content at the given URL
    */
   public String retrieveContentAtURL(URL url) throws IOException {
+      // to make this testable, we should take an interface as an argument that
+      // we could then build a stub interface for in testing. As-is, we can
+      // really only test this at the integration level. It also implies this
+      // method is doing too much - it could/should be decomposed.
       URLConnection connection = url.openConnection();
       InputStream response = connection.getInputStream();
       BufferedReader reader = new BufferedReader(new InputStreamReader(response));
@@ -36,6 +45,37 @@ public class TextClient {
    * @return String the joined string content from retrieving the URLs
    */
   public String retrieveContentAtURLs(ArrayList<URL> urls) {
-   return "foo"; 
+    ExecutorService executorService = Executors.newFixedThreadPool(urls.size());
+
+    ArrayList<CompletableFuture<String>> tasks = new ArrayList<CompletableFuture<String>>(urls.size());
+
+    for(URL url : urls) {
+      CompletableFuture<String> completableFuture = new CompletableFuture<String>();
+      executorService.submit(() -> {
+        try {
+          completableFuture.complete(retrieveContentAtURL(url));
+        }
+        catch(IOException e) {
+          e.printStackTrace();
+        }
+      });
+      tasks.add(completableFuture);
+    }
+
+    // block until every CompletableFuture has completed unfortunately,
+    // CompletableFuture doesn't support collections (at least not that I know
+    // of) so we have to convert o an array
+    CompletableFuture.allOf(tasks.toArray(new CompletableFuture[tasks.size()])).join();
+
+    String results = "";
+    for (CompletableFuture<String> task : tasks) {
+      try {
+        results += task.get();
+      }
+      catch(CancellationException | InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+    return results;
   }
 }
