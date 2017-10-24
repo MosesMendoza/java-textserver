1c34622038d695e0e20e3cf61380a3c864b7d774
Moses Mendoza <moses@puppet.com>
2017-10-24 13:57:37 -0700

    Add integration-style tests for text retrieval
    This tests actually reach out to the internet to download content - not great.
    This is error-prone and could lead to faulty behavior. They should instead be
    operating on interfaces that ensure we're only testing our code and not our
    network connectivity.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- src/test/java/textserver/TextClientTest.java
+++ src/test/java/textserver/TextClientTest.java
@@ -1,10 +1,54 @@
 package textserver;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.net.URL;
+import java.io.IOException;
 
 public class TextClientTest {
   @Test
   public void shouldRetrieveTextAtURL() {
-    
+    /* this is an integration-style test that actually calls out to a known live
+    URL. This would ideally be refactored so we don't need to actually reach out
+    to the internet for this test at a unit-level. */
+    String expected_text = "“Naïvely, the café charged only £1 for a cachaça”.";
+    String site_text;
+    URL url;
+    TextClient client = new TextClient();
+
+    try {
+      url = new URL("http://mackerron.com/text/test.utf8.txt");
+      site_text = client.retrieveContentAtURL(url);
+      assertEquals(site_text, expected_text);
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void shouldRetrieveTextAtURLs() {
+    /* this is an integration-style test that actually calls out to a known live
+    URL. This would ideally be refactored so we don't need to actually reach out
+    to the internet for this test at a unit-level. */
+    String expected_text = "“Naïvely, the café charged only £1 for a cachaça”.“Naïvely, the café charged only £1 for a cachaça”.";
+    String site_text;
+    URL firstUrl;
+    URL secondUrl;
+    TextClient client = new TextClient();
+
+    try {
+      firstUrl = new URL("http://mackerron.com/text/test.utf8.txt");
+      secondUrl = new URL("http://mackerron.com/text/test.utf8.txt");
+      ArrayList<URL> urls = new ArrayList<URL>(2);
+      urls.add(firstUrl);
+      urls.add(secondUrl);
+      site_text = client.retrieveContentAtURLs(urls);
+      assertEquals(site_text, expected_text);
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
   }
 }
